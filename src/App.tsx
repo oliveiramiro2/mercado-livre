2bcff679f12f109e6bd5cd604d4bc46ad551dd5f
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-21 02:11:08 -0300

    added eslint to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="h-screen flex flex-col items-center justify-around">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    return (
+        <div className="h-screen flex flex-col items-center justify-around">
+            <h1>Vite + React</h1>
+            <div className="">
+                <button type="button" onClick={() => setCount(count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="">Click on the Vite and React logos to learn more</p>
+        </div>
+    );
+};
 
-export default App
+export default App;
