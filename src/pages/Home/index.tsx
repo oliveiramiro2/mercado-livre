d6b43234d91e0394ebb85732be3372bfd8748757
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-21 02:14:28 -0300

    added home page to project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+    return (
+        <div className="flex w-screen h-screen items-center justify-center">
+            Home page
+        </div>
+    );
+};
+
+export default Home;
