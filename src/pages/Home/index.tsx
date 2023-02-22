47e05b23feaa234b8509c2e715bc602e7040c9ee
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-22 04:51:45 -0300

    added the section of method of payment

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,7 +11,7 @@ import Header from "../../components/Header";
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col w-screen min-h-screen">
+        <div className="flex flex-col w-screen min-h-screen bg-[#ebebeb]">
             <Header />
             <Swiper
                 spaceBetween={0}
@@ -81,6 +81,80 @@ const Home: React.FC = () => {
                     />
                 </SwiperSlide>
             </Swiper>
+            <div className="w-screen h-[170px] flex justify-center items-center">
+                <div className="bg-white w-[88%] h-[90px] flex">
+                    <div className="w-[260px] h-[90px] border-r border-[#eee] p-1 flex">
+                        <div className="w-1 h-full bg-[#3483fa]" />
+                        <div className="w-full text-left flex flex-col justify-center pl-4">
+                            <p className="text-xl text-[#333]">
+                                Pagamento rápido e seguro
+                            </p>
+                            <p className="text-[#999] text-sm">
+                                com Mercado Pago
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex w-[293px] pl-4 items-center">
+                        <img
+                            src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/credit-card.svg"
+                            alt="cartão crédito"
+                            className="w-12 h-12 cursor-pointer"
+                        />
+                        <div className="pl-4">
+                            <p className="text-xl text-black">
+                                Até 10 parcelas sem juros
+                            </p>
+                            <a
+                                className="text-[#3483fa] text-sm relative bottom-1"
+                                href="https://www.mercadolivre.com.br/gz/home/payments/methods"
+                            >
+                                Ver mais
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex w-[293px] pl-4 items-center">
+                        <img
+                            src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/mercado-creditsv2.svg"
+                            alt="Parcelamento"
+                            className="w-12 h-12 cursor-pointer"
+                        />
+                        <div className="pl-4">
+                            <p className="text-xl text-black">
+                                Parcelamento sem cartão
+                            </p>
+                            <a
+                                className="text-[#3483fa] text-sm relative bottom-1"
+                                href="https://www.mercadolivre.com.br/mercado-credito/boleto-parcelado/?sk=RHRsnBTf"
+                            >
+                                Conheça o Mercado Crédito
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex w-[233px] pl-4 items-center">
+                        <img
+                            src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/qr.svg"
+                            alt="Via PIX"
+                            className="w-12 h-12 cursor-pointer"
+                        />
+                        <div className="pl-4">
+                            <p className="text-xl text-black">Via Pix</p>
+                            <a
+                                className="text-[#3483fa] text-sm relative bottom-1"
+                                href="https://www.mercadolivre.com.br/gz/home/payments/methods#pix"
+                            >
+                                Ver mais
+                            </a>
+                        </div>
+                    </div>
+                    <div className="w-[130px] flex items-center justify-center">
+                        <img
+                            src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/view-more.svg"
+                            alt="Mais"
+                            className="w-12 h-12 cursor-pointer"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
