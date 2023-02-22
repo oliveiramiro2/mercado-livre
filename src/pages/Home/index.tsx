4cab8442fe3f4152897f6d3c487b1495a6914926
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-22 03:01:28 -0300

    swiper libs is working but still have some problems

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,85 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 import Header from "../../components/Header";
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col w-screen h-screen items-center">
+        <div className="flex flex-col w-screen min-h-screen">
             <Header />
-            Home page
+            <Swiper
+                spaceBetween={0}
+                slidesPerView="auto"
+                modules={[Navigation, Pagination, Autoplay]}
+                navigation
+                pagination={{
+                    clickable: true,
+                    bulletActiveClass: "scale-150",
+                }}
+                loop
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: true,
+                    pauseOnMouseEnter: true,
+                }}
+                className="h-[340px]"
+            >
+                <SwiperSlide>
+                    <img
+                        className="h-[340px]"
+                        src="https://http2.mlstatic.com/D_NQ_698675-MLA53861274292_022023-OO.webp"
+                        alt="bbb23"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="h-[340px]"
+                        src="https://http2.mlstatic.com/D_NQ_973121-MLA53828765774_022023-OO.webp"
+                        alt="Aproveite o verão"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="h-[340px]"
+                        src="https://http2.mlstatic.com/D_NQ_939429-MLA53879648181_022023-OO.webp"
+                        alt="Detox"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="h-[340px]"
+                        src="https://http2.mlstatic.com/D_NQ_989972-MLA53831254570_022023-OO.webp"
+                        alt="Grandes marcas"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="h-[340px]"
+                        src="https://http2.mlstatic.com/D_NQ_634797-MLA53858146978_022023-OO.webp"
+                        alt="Gamers"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="h-[340px]"
+                        src="https://http2.mlstatic.com/D_NQ_858900-MLA53880103142_022023-OO.webp"
+                        alt="Smartphone"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="h-[340px]"
+                        src="https://http2.mlstatic.com/D_NQ_937944-MLA53924707389_022023-OO.webp"
+                        alt="Ofertas do dia"
+                    />
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
