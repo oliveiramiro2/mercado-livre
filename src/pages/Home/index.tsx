e20ecd104985f915e2d7763ba541894b4d67ca4c
oliveiramiro2 <ramiromares95@gmail.com>
2023-02-23 04:48:09 -0300

    added marisa section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import { Navigation, Pagination, Autoplay } from "swiper";
 import clsx from "clsx";
 import gsap from "gsap";
 import { FcNext, FcPrevious } from "react-icons/fc";
+import { MdNavigateNext } from "react-icons/md";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -656,6 +657,40 @@ const Home: React.FC = () => {
                     >
                         {showLoyalty4 ? <FcPrevious /> : <FcNext />}
                     </button>
+                </div>
+            </section>
+            <section className="w-screen h-[330px]">
+                <div className="flex items-center justify-center cursor-pointer">
+                    <div className="w-[87%] h-[250px] flex">
+                        <img
+                            src="https://http2.mlstatic.com/D_NQ_945960-MLA53711315255_022023-OO.jpg"
+                            alt="Mulheres"
+                            className="w-[700px] h-[250px] rounded-l-[4px]"
+                        />
+                        <div className="w-[480px] h-[250px] p-16 bg-[#8533af] rounded-r-[4px]">
+                            <div className="flex flex-col gap-y-7">
+                                <div>
+                                    <p className="text-white text-2xl font-semibold">
+                                        MÊS DE OFERTAS
+                                    </p>
+                                    <p className="text-white text-2xl font-semibold">
+                                        DESCONTOS ATÉ 40%
+                                    </p>
+                                </div>
+                                <p className="text-white text-lg font-normal flex items-center gap-x-3">
+                                    Confira{" "}
+                                    <MdNavigateNext size={22} color="#fff" />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative bottom-40 left-[150px] p-3 pl-4 w-32 bg-white">
+                    <img
+                        src="https://http2.mlstatic.com/D_NQ_733938-MLA53711303917_022023-OO.jpg"
+                        alt="Marisa"
+                        className="w-24 h-16"
+                    />
                 </div>
             </section>
         </div>
