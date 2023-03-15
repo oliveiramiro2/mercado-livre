86793139fa9767de87955d5e06e865f49f656d53
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-15 01:45:06 -0300

    put the code of section inside file brand offer
    refator and responsivity brand marisa

--- src/components/BrandOffer/index.tsx
+++ src/components/BrandOffer/index.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { MdNavigateNext } from "react-icons/md";
 
-const BrandOffer: React.FC = () => {
-  return (
-    <div>index</div>
-  )
-}
+const BrandOffer: React.FC = () => (
+    <section className="w-screen h-[380px] pt-16">
+        <div className="flex items-center justify-center cursor-pointer">
+            <div className="w-[87%] h-[250px] flex">
+                <img
+                    src="https://http2.mlstatic.com/D_NQ_945960-MLA53711315255_022023-OO.jpg"
+                    alt="Mulheres"
+                    className="w-[700px] h-[250px] rounded-l-[4px]"
+                />
+                <div className="w-[480px] h-[250px] p-16 bg-[#8533af] rounded-r-[4px]">
+                    <div className="flex flex-col gap-y-7">
+                        <div>
+                            <p className="text-white text-2xl font-semibold">
+                                MÊS DE OFERTAS
+                            </p>
+                            <p className="text-white text-2xl font-semibold">
+                                DESCONTOS ATÉ 40%
+                            </p>
+                        </div>
+                        <p className="text-white text-lg font-normal flex items-center gap-x-3">
+                            Confira <MdNavigateNext size={22} color="#fff" />
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="relative bottom-40 left-[150px] p-3 pl-4 w-32 bg-white">
+            <img
+                src="https://http2.mlstatic.com/D_NQ_733938-MLA53711303917_022023-OO.jpg"
+                alt="Marisa"
+                className="w-24 h-16"
+            />
+        </div>
+    </section>
+);
 
-export default BrandOffer
+export default BrandOffer;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,6 @@ import {
     GiHeartPlus,
 } from "react-icons/gi";
 import {
-    MdNavigateNext,
     MdPets,
     MdPalette,
     MdEngineering,
@@ -155,40 +154,7 @@ const Home: React.FC = () => {
             <TodayOffers />
             <LevelSix />
             <Benefits />
-            <section className="w-screen h-[380px] pt-16">
-                <div className="flex items-center justify-center cursor-pointer">
-                    <div className="w-[87%] h-[250px] flex">
-                        <img
-                            src="https://http2.mlstatic.com/D_NQ_945960-MLA53711315255_022023-OO.jpg"
-                            alt="Mulheres"
-                            className="w-[700px] h-[250px] rounded-l-[4px]"
-                        />
-                        <div className="w-[480px] h-[250px] p-16 bg-[#8533af] rounded-r-[4px]">
-                            <div className="flex flex-col gap-y-7">
-                                <div>
-                                    <p className="text-white text-2xl font-semibold">
-                                        MÊS DE OFERTAS
-                                    </p>
-                                    <p className="text-white text-2xl font-semibold">
-                                        DESCONTOS ATÉ 40%
-                                    </p>
-                                </div>
-                                <p className="text-white text-lg font-normal flex items-center gap-x-3">
-                                    Confira{" "}
-                                    <MdNavigateNext size={22} color="#fff" />
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="relative bottom-40 left-[150px] p-3 pl-4 w-32 bg-white">
-                    <img
-                        src="https://http2.mlstatic.com/D_NQ_733938-MLA53711303917_022023-OO.jpg"
-                        alt="Marisa"
-                        className="w-24 h-16"
-                    />
-                </div>
-            </section>
+
             <section className="w-screen h-[399px] flex items-center justify-center">
                 <div className="h-[318px] w-[87%]">
                     <div className="pl-1 mb-[22px]">
