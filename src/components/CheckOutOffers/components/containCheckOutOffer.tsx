d857d17dc63d492d3cd34d86ec57fd72dbaea7ff
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-15 02:15:43 -0300

    take the code on contain check out offer
    refator and responsivity check out offers

--- src/components/CheckOutOffers/components/containCheckOutOffer.tsx
+++ src/components/CheckOutOffers/components/containCheckOutOffer.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { IPropsCheckOutOffer } from "../../../interfaces";
 
-const ContainCheckOutOffer = () => {
-  return (
-    <div>containCheckOutOffer</div>
-  )
-}
+const ContainCheckOutOffer: React.FC<IPropsCheckOutOffer> = ({
+    alt,
+    link,
+    text1,
+    text2,
+    text3,
+}) => (
+    <div className="rounded-[4px] w-[584px] h-[250px] bg-white flex justify-between items-center hover:shadow-xl hover:shadow-slate-500 transition-shadow ease-in-out delay-100 cursor-pointer">
+        <div className="w-[230px] h-[145px] flex flex-col justify-center text-start ml-8">
+            <span className="text-[#4b4b4b] font-semibold mb-1 text-[11px] tracking-[5px]">
+                {text1}
+            </span>
+            <span className="text-[28px] font-semibold text-[#4b4b4b]">
+                {text2}
+            </span>
+            <span className="text-[28px] relative bottom-2 font-semibold text-[#4b4b4b]">
+                {text3}
+            </span>
+            <button
+                className="self-start w-24 h-12 rounded-sm bg-pallet-blue text-white font-semibold hover:bg-[#2a6bcc] transition-colors ease-in-out delay-75"
+                type="button"
+            >
+                Confira
+            </button>
+        </div>
+        <img
+            src={link}
+            alt={alt}
+            className="w-[300px] h-[250px] rounded-r-[4px]"
+        />
+    </div>
+);
 
 export default ContainCheckOutOffer
