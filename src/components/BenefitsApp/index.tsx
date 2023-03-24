bc923ac4815e73b3b18ad93c9ac8ca383d8a13d3
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-24 00:31:44 -0300

    call components into benefits app and remove old code
    refator benefits app

--- src/components/BenefitsApp/index.tsx
+++ src/components/BenefitsApp/index.tsx
@@ -1,43 +1,7 @@
 import React from "react";
-import clsx from "clsx";
 
-import { IPropsContainBenefitsApp } from "../../interfaces";
 import { allBenefitsApp } from "../../utils/vars";
-
-const ContainBenefitsApp: React.FC<IPropsContainBenefitsApp> = ({
-    alt,
-    content,
-    link,
-    linkBenefit,
-    title,
-    id,
-}) => (
-    <div
-        className={clsx("flex flex-col items-center w-[23%]", {
-            "w-[29%]": id === 1,
-        })}
-    >
-        <img src={link} alt={alt} className="h-[20%] mb-5" />
-        <div className="flex flex-col items-center gap-y-2">
-            <h2 className="font-proxima text-[#4b4b4b] text-xl">{title}</h2>
-            <span className={clsx("text-center font-proxima text-[#999] text-[15px]",{
-                "border-r": id === 1,
-                "border-l": id === 1,
-                "border-gray": id === 1,
-                "pr-16": id === 1,
-                "pl-16": id === 1,
-            })}>
-                {content}
-            </span>
-            <a
-                href="https://www.mercadolivre.com.br/"
-                className="text-pallet-blue relative -bottom-5 font-proxima"
-            >
-                {linkBenefit}
-            </a>
-        </div>
-    </div>
-);
+import ContainBenefitsApp from "./components/containBenefitsApp";
 
 const BenefitsApp: React.FC = () => (
     <section className="w-screen mt-20 bg-white h-[300px] pt-10 pb-8 flex justify-evenly">
